1ab01c14b928d17d77e98e1b7b867712622a070b
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-09 10:12:29 +0600

    Update circle.java
    class Circle

--- circle.java
+++ circle.java
@@ -8,3 +8,27 @@ public class Main {
         System.out.println("Circle's area :" + circle.getArea()+ " Cercle's size" + circle.getCercumference());
     }
 }
+class Circle {
+    private double radious;
+
+    Circle(){
+    }
+    Circle(double radious){
+        this.radious = radious;
+    }
+    public double getRadious(){
+        return radious;
+    }
+    public  void setRadious(double new_radious){
+        this.radious = new_radious;
+    }
+    public double getArea(){
+        return 2 * Math.PI * Math.pow(radious, 2);
+    }
+ public double getCercumference(){
+        return 2 * Math.PI * radious;
+ }
+ public String toString(){
+        return "Circle [" + radious + "]";
+ }
+}
