553e685a620367664a2e20f05ef02f4de56f5e38
Aiymgul01 <124436362+Aiymgul01@users.noreply.github.com>
2023-02-11 03:12:54 +0600

    Create Time.java

---
+++ .github/Time.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public static void main (String[]args){
+ Scanner scan = new Scanner(System.in);
+    
+
+}
+Time{
+    private int hour;
+    private int minute;
+    private int second;
+    Time(int h, int m, int s){
+        this.hour = h;
+        this.minute = m;
+        this.secong = s;
+    }
+    public int getHour(){
+        return hour;
+    }
+    public int getMinute(){
+        return minute;
+    }
+    public int getSecond()(
+        return second;
+    )
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+    public void setSecond(int second){
+        this.ssecond = second;
+    }
+    public void setTime(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    public String toString(){
+        return String.format(format:"%d:%d:%d:", hour, mimute,second);
+    }
+    public 
+}
